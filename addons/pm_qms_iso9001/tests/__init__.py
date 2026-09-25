94f77458fcbf850473413ca766bf1182b84e9a58
BigPortilloUS <info@cloudpixelstudio.agency>
2026-09-25 17:32:45 -0400

    Load ISO gap assessment tests

--- addons/pm_qms_iso9001/tests/__init__.py
+++ addons/pm_qms_iso9001/tests/__init__.py
@@ -3,3 +3,4 @@ from . import test_initial_implementation
 from . import test_guided_content
 from . import test_amendment1
 from . import test_transition_scenarios
+from . import test_gap_assessment
